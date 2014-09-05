c79a3e4c11cffc34667c13e7bcb9e67bf6af78cd
Sergey <rsv-astana@mail.ru>
2014-09-05 17:29:04 +0600

    Получилось получить список книг с сылками с одной страницы.

--- BooksDownloader/Program.cs
+++ BooksDownloader/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Xml.Linq;
 
 namespace BooksDownloader
 {
@@ -13,16 +14,50 @@ namespace BooksDownloader
         {
             Console.WriteLine("BOOKS READER");
 
-            string url = "http://knigosite.org/library/genres/110/page/1";
+            string url = "http://knigosite.org/library/genres/113/page/1";
             string html = String.Empty;
             HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(url);
             HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
-            StreamReader stream = new StreamReader(myResponse.GetResponseStream(), Encoding.GetEncoding(1251));
+            StreamReader stream = new StreamReader(myResponse.GetResponseStream());
             html = stream.ReadToEnd();
             //File.Create(@"C:\Users\Сергей\Desktop\1.xml");
             
-            File.WriteAllText(@"\1.xml", html);
+            //File.WriteAllText(@"\1.xml", html);
             Console.WriteLine(html);
+
+            XDocument doc = XDocument.Load(@"\1.xml");
+            var els = doc.Root.Element("body").Element("div").Elements();
+            XElement mainDiv = null;
+            foreach (var xElement in els)
+            {
+                if (xElement.Attribute("class").Value == "main  library genres")
+                {
+                    mainDiv = xElement;
+                    break;
+                }
+            }
+            els = mainDiv.Element("div").Element("div").Elements();
+            XElement ul = null;
+            foreach (var xElement in els)
+            {
+                if (xElement.HasAttributes && xElement.Attribute("class").Value == "lib_books_list books")
+                {
+                    ul = xElement;
+                    break;
+                }
+            }
+            els = ul.Elements();
+            Dictionary<string, string> links = new Dictionary<string, string>();
+            foreach (var xElement in els)
+            {
+                var link = xElement.Element("p").Element("a");
+                links.Add(link.Attribute("title").Value, link.Attribute("href").Value);
+            }
+
+            foreach (var link in links)
+            {
+                Console.WriteLine(link);
+            }
 
             Console.Read();
         }
